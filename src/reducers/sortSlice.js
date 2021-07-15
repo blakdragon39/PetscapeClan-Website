9c92c1b3dc5da2eee7185463d709184fa1985af3
Jenna Link <jenna.hartley@pushinteractions.com>
2021-07-15 16:35:17 -0600

    Setting a default sort type

--- src/reducers/sortSlice.js
+++ src/reducers/sortSlice.js
@@ -1,10 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit'
 import localStorage from '../services/localStorage'
+import sortTypes from '../utils/sortTypes'
 
 const sortSlice = createSlice({
     name: 'sort',
     initialState: {
-        value: localStorage.getSortType()
+        value: localStorage.getSortType() ? localStorage.getSortType() : sortTypes[0].label
     },
     reducers: {
         setSortType: (state, action) => {
